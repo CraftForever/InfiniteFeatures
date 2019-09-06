ce6772ee7f827490e1df369f29a281698f1adf1c
halotroop2288 <carolinejoybell@gmail.com>
2019-09-06 12:33:23 -0700

    Adds templates for items and blocks
    This should be a temporary measure until we actually make a custom
    registry. Forge Team might not like the idea, but it would be a more
    efficient solution.

--- src/main/java/com/github/craftforever/infinitefeatures/InfiniteFeatures.java
+++ src/main/java/com/github/craftforever/infinitefeatures/InfiniteFeatures.java
@@ -5,25 +5,45 @@ import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
-@Mod(modid = "infeatures")
+@Mod(modid = "infeatures", version = "1.0.0", acceptedMinecraftVersions = "1.12.2")
 public class InfiniteFeatures
 {
-	
 	public static final String modID = "infeatures";
+	public static final String namespace = modID+":";
 	public static World world;
 	public static File saveDir;
 	public static Logger logger = LogManager.getLogger();
+	
+	public static CreativeTabs infItemsTab = new CreativeTabs("infeatures_items_tab")
+	{
+		@Override
+		public ItemStack createIcon() {	return new ItemStack(Templates.items()[0]);	}
+	};
+	
+	public static CreativeTabs infBlocksTab = new CreativeTabs("infeatures_blocks_tab")
+	{
+		@Override
+		public ItemStack createIcon() {	return new ItemStack(Templates.blocks()[0]);	}
+	};
+	
 
 	public InfiniteFeatures()
 	{
 		MinecraftForge.EVENT_BUS.register(getClass());
 	}
+	
 	@SubscribeEvent
 	public static void onWorldLoad(WorldEvent.Load event) throws IOException
 	{
@@ -33,13 +53,31 @@ public class InfiniteFeatures
 		
 		if(!generationFile.exists())
 		{
+			// TODO place randomization functions here
+			// Save them to generationFile before creating.
+
 			generationFile.createNewFile();
-			logger.debug("File generated here: " + generationFile.toPath().toString());
+			logger.info("File generated here: " + generationFile.toPath().toString());
 		}
 		else
 		{
-			logger.debug("File already exists: " + generationFile.toPath().toString());
+			logger.info("File already exists: " + generationFile.toPath().toString());
 		}
 		
+		// TODO load objects from file to the registry.
 	}
+	
+	@SubscribeEvent
+	public static void registerBlocks(RegistryEvent<Block> event)
+	{
+		ForgeRegistries.BLOCKS.registerAll(Templates.blocks());
+		ForgeRegistries.ITEMS.registerAll(Templates.blockItems());
+	}
+	
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent<Item> event)
+	{
+		ForgeRegistries.ITEMS.registerAll(Templates.items());
+	}
+	
 }
